f386de5b416782115adb60d9e836aa779e56f987
luc1dLife <luc1d_life@protonmail.com>
2020-10-04 19:43:21 -0600

    Completed Player Object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,6 +2,33 @@ const Potion = require('../lib/Potion');
 const Player = require('../lib/Player');
 jest.mock('../lib/Potion');
 
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
+  });
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+  
+    player.addPotion(new Potion());
+  
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+  });
+
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+  
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+  })
+
 test("subtracts from player's health", () => {
     const player = new Player('Dave');
     const oldHealth = player.health;
